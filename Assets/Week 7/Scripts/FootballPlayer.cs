48cc7107d5a1dfa01b4ff3e85a94b3d88fa414f0
SHERNET\barryki <kit.barry@sheridancollege.ca>
2024-02-22 14:21:39 -0500

    Task 1b complete can select players

---
+++ Assets/Week 7/Scripts/FootballPlayer.cs
@@ -0,0 +1,35 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootballPlayer : MonoBehaviour
+{
+    SpriteRenderer sr;
+    public Color selectedColor;
+    public Color unselectedColor;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        Selected(false);
+    }
+
+    private void OnMouseDown()
+    {
+        Selected(true);
+    }
+
+    public void Selected(bool isSelected)
+    {
+        if (isSelected)
+        {
+            sr.color = selectedColor;
+        }
+        else
+        {
+            sr.color = unselectedColor;
+        }
+    }
+}
